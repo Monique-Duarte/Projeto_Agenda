db1f319985e15377722a1c06e220b587e8adb9ba
Monique Duarte <moniquead95@gmail.com>
2024-12-19 11:31:28 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const form= document.getElementById('form');
+const name = [];
+const tell= [];
+
+let line = '';
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    addLine();
+    updateTable();
+    });
+
+function addLine(){
+    const inputName = document.getElementById('name');
+    const inputTell = document.getElementById('ContactNumber');
+
+    if (name.includes(inputName.value)) {
+        alert(`O contato ${inputName.value} já foi salvo`);
+    } else {
+
+    name.push(inputName.value);
+    tell.push(toString(inputTell.value));
+
+    let row = '<tr>'; 
+    row += `<td>${inputName.value}</td>`;
+    row += `<td>${inputTell.value}</td>`;
+        row += '</tr>';
+
+    line += row;
+    }
+
+    inputName.value = '';
+    inputTell.value = '';
+}
+
+function updateTable() {
+    const bodyTable = document.querySelector('tbody');
+    bodyTable.innerHTML = line;
+}
